9c100819bfcd6e729dfe337f61ac05a3a09da11a
dannycrews <dancrews@me.com>
2016-08-29 12:55:24 -0400

    Pass in props. Add defaultProps

--- complete-react/HelloReact/public/app.jsx
+++ complete-react/HelloReact/public/app.jsx
@@ -1,15 +1,27 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is a default prop',
+    };
+  },
+
   render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is from a component</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
       </div>
     );
   },
 });
 
+var firstName = 'Dan';
+var myMessage = 'This is from a component';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={myMessage}/>,
   document.getElementById('app')
 );
